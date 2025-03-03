7f3ae4d8e9c3ed6e6943a8ac1461663081cadc53
Mdnkosi <mahlatsedineo93@gmail.com>
2025-03-03 18:16:18 +0200

    Added the search functionalities using the API.

--- src/java.js
+++ src/java.js
@@ -1,9 +1,32 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+ 
+}
+
+
+
+function searchCity(city) {
+  let apiKey = "t0dbadd020obaf40a394084940d3fadf";
+  let apiUrl =
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Polokwane");
